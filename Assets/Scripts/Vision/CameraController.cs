9610f9d3dab6b9b9d00ddf24e4f2a986d64f8383
Rishabh Garg <narishabh.garg@gmail.com>
2025-09-06 21:20:26 +0530

    Changed Player Model & converted project to URP

--- Assets/Scripts/Vision/CameraController.cs
+++ Assets/Scripts/Vision/CameraController.cs
@@ -1,44 +1,41 @@
 using UnityEngine;
 
-namespace Vision
+public class CameraController : MonoBehaviour
 {
-    public class CameraController : MonoBehaviour
+    [SerializeField] private Transform followTarget;
+    [SerializeField] private Vector3 followDistanceVector = new Vector3(0f, 1f, -3f);
+    [SerializeField] private Vector2 verticalRotationAngleRange = new Vector2(-30f, 15f);
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private bool invertMouseXAxis = false;
+    [SerializeField] private bool invertMouseYAxis = false;
+
+    private float rotationX;
+    private float rotationY;
+
+    private void Start()
     {
-        [SerializeField] private Transform followTarget;
-        [SerializeField] private Vector3 followDistanceVector = new Vector3(0f, 1f, -3f);
-        [SerializeField] private Vector2 verticalRotationAngleRange = new Vector2(-30f, 15f);
-        [SerializeField] private float mouseSensitivity = 100f;
-        [SerializeField] private bool invertMouseXAxis = false;
-        [SerializeField] private bool invertMouseYAxis = false;
-
-        private float rotationX;
-        private float rotationY;
-
-        private void Start()
-        {
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-
-        private void LateUpdate()
-        {
-            SetCamera();
-        }
-
-        private void SetCamera()
-        {
-            int invertMouseYAxisVal = invertMouseYAxis ? -1 : 1;
-            rotationX += Input.GetAxis("Mouse Y") * mouseSensitivity * invertMouseYAxisVal * Time.deltaTime;
-            rotationX = Mathf.Clamp(rotationX, verticalRotationAngleRange.x, verticalRotationAngleRange.y);
-
-            int invertMouseXAxisVal = invertMouseXAxis ? -1 : 1;
-            rotationY += Input.GetAxis("Mouse X") * mouseSensitivity * invertMouseXAxisVal * Time.deltaTime;
-
-            Quaternion targetRotation = Quaternion.Euler(-rotationX, rotationY, 0f);
-            transform.position = followTarget.position + targetRotation * followDistanceVector;
-            transform.rotation = targetRotation;
-        }
-
-        public Quaternion GetPlanarRotation() => Quaternion.Euler(0f, rotationY, 0f);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
+    private void LateUpdate()
+    {
+        SetCamera();
+    }
+
+    private void SetCamera()
+    {
+        int invertMouseYAxisVal = invertMouseYAxis ? -1 : 1;
+        rotationX += Input.GetAxis("Mouse Y") * mouseSensitivity * invertMouseYAxisVal * Time.deltaTime;
+        rotationX = Mathf.Clamp(rotationX, verticalRotationAngleRange.x, verticalRotationAngleRange.y);
+
+        int invertMouseXAxisVal = invertMouseXAxis ? -1 : 1;
+        rotationY += Input.GetAxis("Mouse X") * mouseSensitivity * invertMouseXAxisVal * Time.deltaTime;
+
+        Quaternion targetRotation = Quaternion.Euler(-rotationX, rotationY, 0f);
+        transform.position = followTarget.position + targetRotation * followDistanceVector;
+        transform.rotation = targetRotation;
+    }
+
+    public Quaternion GetPlanarRotation() => Quaternion.Euler(0f, rotationY, 0f);
 }
